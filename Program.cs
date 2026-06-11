eea1c0b44cc2fdeffd2f4704d12a015914e79b08
Alisson Zimmer <alisson.zimmer@edu.sc.senai.br>
2026-06-10 22:13:48 -0300

    contador de 1 a 5

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-﻿// Escreva a mensagem "Informe um numero: "
-Console.Write("Informe um numero: ");
-// Cria a variável variavelQueSalvaUmNumero, 
-// leia o que o usuário digitar
-// e tranforma o valor lido de texto para inteiro (int.Parse)
-int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
-
-// verifica se o valor de variavelQueSalvaUmNumero é par
-if (variavelQueSalvaUmNumero % 2 == 0) {
-    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
-} else {
-    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é ímpar");
+﻿for (int i = 1; i <= 5; i++) {
+    Console.WriteLine($"{i}");
 }
