e4b2305ec5366d7d19ba70d5989e8b8b6d2662a4
Alisson Zimmer <alisson.zimmer@edu.sc.senai.br>
2026-06-10 21:52:15 -0300

    par e impar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Escreva a mensagem "Informe um numero: "
+Console.Write("Informe um numero: ");
+// Cria a variável variavelQueSalvaUmNumero, 
+// leia o que o usuário digitar
+// e tranforma o valor lido de texto para inteiro (int.Parse)
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
+
+// verifica se o valor de variavelQueSalvaUmNumero é par
+if (variavelQueSalvaUmNumero % 2 == 0) {
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é par");
+} else {
+    Console.WriteLine($"Número {variavelQueSalvaUmNumero} é ímpar");
+}
